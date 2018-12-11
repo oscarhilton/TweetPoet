907c66f56211ddc37a04eb290a47d994926b4462
Oscar Hilton <oscar.hilton@mobile-5.com>
2018-12-11 18:07:07 +0000

    Changes

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const getRhymes = require("get-rhymes");
-var Tokenizer = require("sentence-tokenizer");
-var syllable = require("syllable");
-var { tweets } = require("./tweets");
+const Tokenizer = require("sentence-tokenizer");
+const syllable = require("syllable");
+const { tweets } = require("./tweets");
+const axios = require("axios");
+const readlineSync = require("readline-sync");
+const chalk = require("chalk");
 
 // Set up user's line
-const sentence = "Tell me it's not true";
-const splitSentence = sentence.split(" ");
-const wordToBeRhymed = splitSentence[splitSentence.length - 1];
+const myArgs = process.argv.slice(2);
+const sentence = myArgs.join(" ");
+
+let thePoem = [];
 
 const getSentenceSyllables = sentenceToUse => {
   const split = sentenceToUse.split(" ");
@@ -43,13 +47,12 @@ cleanTweets = cleanTweets.map(tweet => {
       return tweet.replace("has", "I have");
     case "isnt":
       return tweet.replace("isnt", "I'm not");
-    case "is":
-      return tweet.replace("is", "I'm");
     default:
       return tweet;
   }
 });
 
+// Put tweets into rhyme object
 const rhymeTweets = cleanTweets.map(tweet => {
   const splitTweet = tweet.split(" ");
   return {
@@ -58,36 +61,85 @@ const rhymeTweets = cleanTweets.map(tweet => {
   };
 });
 
-getRhymes(wordToBeRhymed)
-  .then(rhymes => {
-    if (rhymeTweets.length < 1) {
-      return console.warn("No rhymeTweets found");
-    }
-    const found = rhymeTweets.filter(
-      tweet =>
-        rhymes.indexOf(tweet.lastWord) > -1 &&
-        tweet.lastWord !== wordToBeRhymed &&
-        !wordToBeRhymed.includes(tweet.lastWord) &&
-        !tweet.lastWord.includes(wordToBeRhymed) &&
-        getSyllableRange(
-          getSentenceSyllables(sentence),
-          getSentenceSyllables(tweet.tweet)
-        )
+// Call the rhyme API
+const getThingsThatRhymeWith = async function(word) {
+  try {
+    const res = await axios.get(
+      `https://api.datamuse.com/words?rel_rhy=${word}&max=1000`
     );
-    if (found.length < 1) return console.log("No foundtweets found");
-    return outputNextLine(found);
-  })
-  .catch(e => console.warn(e));
+    return res.data.map(word => word.word);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const LargeConsole = log => {
+  console.log(chalk.green("********************************"));
+  console.log("                           ");
+  console.log("                           ");
+  log.map(line => console.log(chalk.yellow(line)));
+  console.log("                           ");
+  console.log("                           ");
+  console.log(chalk.green("********************************"));
+};
 
 // Make sentence titlecase
 function toTitleCase(str) {
   return str.replace(str[0], str[0].toUpperCase());
 }
 
-const outputNextLine = foundArray => {
-  const randomLine = foundArray[Math.floor(Math.random() * foundArray.length)];
-  // console.log(foundArray, randomLine);
+// Make a new line of the poem
+const startPoemLine = () => {
+  var sentence = readlineSync.question("*** Input a line to the poem: *** ");
 
-  console.log(toTitleCase(sentence));
-  console.log(toTitleCase(randomLine.tweet));
+  const splitSentence = sentence.split(" ");
+  const wordToBeRhymed = splitSentence[splitSentence.length - 1];
+
+  getThingsThatRhymeWith(wordToBeRhymed)
+    .then(rhymes => {
+      if (rhymeTweets.length < 1) {
+        return console.warn("No rhymeTweets found");
+      }
+
+      const found = rhymeTweets.filter(
+        tweet =>
+          rhymes.indexOf(tweet.lastWord) !== -1 &&
+          tweet.lastWord !== wordToBeRhymed &&
+          !wordToBeRhymed.includes(tweet.lastWord) &&
+          !tweet.lastWord.includes(wordToBeRhymed) &&
+          getSyllableRange(
+            getSentenceSyllables(sentence),
+            getSentenceSyllables(tweet.tweet)
+          )
+      );
+      if (found.length < 1) {
+        console.log(chalk.red("Oh no, no good rhyme for that line :-("));
+        return startPoemLine();
+      }
+      const randomLine = found[Math.floor(Math.random() * found.length)];
+      return addToPoem(toTitleCase(sentence), toTitleCase(randomLine.tweet));
+    })
+    .catch(e => console.warn(e));
 };
+
+// Add the line to the poem
+const addToPoem = (line1, line2) => {
+  thePoem.push(line1);
+  thePoem.push(line2);
+
+  LargeConsole(thePoem);
+
+  if (readlineSync.keyInYN("Do you want to finish the poem?")) {
+    console.log("Poem finished!");
+    thePoem = [];
+    if (readlineSync.keyInYN("Want to write another poem?")) {
+      startPoemLine();
+    } else {
+      console.log("Bye bye!");
+    }
+  } else {
+    startPoemLine();
+  }
+};
+
+startPoemLine();
